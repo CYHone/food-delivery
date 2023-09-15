6097431940d8f8b322002efe56b31b3b56b9bd35
CYH <3243773507@qq.com>
2023-09-15 12:36:11 +0800

    新增套餐业务代码开发
    Date:      Fri Sep 15 12:36:11 2023 +0800

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,6 @@
 package com.sky.mapper;
 
+import com.sky.entity.SetmealDish;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -13,4 +14,10 @@ public interface SetmealDishMapper {
      */
     //动态SQL
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+
+    /**
+     * 批量保存套餐和菜品的关联关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
 }
