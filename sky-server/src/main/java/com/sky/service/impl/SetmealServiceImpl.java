9006470bf5fb52b22a6856315a56a1cbb090bc1d
CYH <3243773507@qq.com>
2023-10-11 15:56:21 +0800

    店铺营业状态设置业务代码开发
    Date:      Wed Oct 11 15:56:21 2023 +0800

--- sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -16,6 +16,7 @@ import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.result.PageResult;
 import com.sky.service.SetmealService;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.SetmealVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
@@ -24,6 +25,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+
+
 
     /**
      * 套餐业务实现
@@ -168,5 +171,27 @@ import java.util.List;
                     .build();
             setmealMapper.update(setmeal);
         }
+
+        /**
+         * 条件查询
+         * @param setmeal
+         * @return
+         */
+        public List<Setmeal> list(Setmeal setmeal) {
+            List<Setmeal> list = setmealMapper.list(setmeal);
+            return list;
+        }
+
+        /**
+         * 根据id查询菜品选项
+         * @param id
+         * @return
+         */
+        public List<DishItemVO> getDishItemById(Long id) {
+            return setmealMapper.getDishItemBySetmealId(id);
+        }
+
+
+
     }
 
