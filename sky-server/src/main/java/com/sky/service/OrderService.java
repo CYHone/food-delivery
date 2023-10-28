ad567028a76958c66e927e5de34c341f24b587d4
CYH <3243773507@qq.com>
2023-10-28 10:22:23 +0800

    再来一单代码开发
    Date:      Sat Oct 28 10:22:23 2023 +0800

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -50,4 +50,11 @@ public interface OrderService {
      */
     void userCancelById(Long id) throws Exception;
 
+
+    /**
+     * 再来一单
+     *
+     * @param id
+     */
+    void repetition(Long id);
 }
