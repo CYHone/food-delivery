a711afc4a66d0ac38a8c1fe4b82b7c48c9d76267
CYH <3243773507@qq.com>
2023-11-01 16:55:11 +0800

    商家端订单搜索业务代码开发
    Date:      Wed Nov 1 16:55:11 2023 +0800

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -57,4 +57,12 @@ public interface OrderService {
      * @param id
      */
     void repetition(Long id);
+
+
+    /**
+     * 条件搜索订单
+     * @param ordersPageQueryDTO
+     * @return
+     */
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 }
