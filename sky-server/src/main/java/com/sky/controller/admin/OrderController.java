cd8d6c2dce14dfd78e1ce3f95a57e31ceab8aeed
CYH <3243773507@qq.com>
2023-11-05 12:21:01 +0800

    商家端取消订单业务代码开发
    Date:      Sun Nov 5 12:21:01 2023 +0800

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -1,5 +1,6 @@
 package com.sky.controller.admin;
 
+import com.sky.dto.OrdersCancelDTO;
 import com.sky.dto.OrdersConfirmDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.result.PageResult;
@@ -75,6 +76,19 @@ import org.springframework.web.bind.annotation.*;
             orderService.confirm(ordersConfirmDTO);
             return Result.success();
         }
+
+
+    /**
+     * 取消订单
+     *
+     * @return
+     */
+    @PutMapping("/cancel")
+    @ApiOperation("取消订单")
+    public Result cancel(@RequestBody OrdersCancelDTO ordersCancelDTO) throws Exception {
+        orderService.cancel(ordersCancelDTO);
+        return Result.success();
+    }
     }
 
 
